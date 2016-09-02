f8975042be67942a5c0666b0e9c8165d0472aa39
Zhang Qiang <zhangqiangoffice@163.com>
2016-09-02 09:55:33 +0800

    终于能跑起来了

--- app/src/main/java/com/zhanglemeng/www/f2503/base/HomeFragment.java
+++ app/src/main/java/com/zhanglemeng/www/f2503/base/HomeFragment.java
@@ -1,41 +1,28 @@
 package com.zhanglemeng.www.f2503.base;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.zhanglemeng.www.f2503.R;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * Created by Administrator on 2016/4/27.
+ * Created by 无名大强 on 2016/9/2.
  * 首页界面
  */
-public class HomeFragment extends Fragment implements View.OnClickListener{
+public class HomeFragment extends Fragment {
     private View convertView;
-    private TextView tv_concat_launch,tv_concat_join;
-    private View view_concat_launch,view_concat_join;
-    private RelativeLayout rl_home,rl_job;
-    private ImageView iv_search,iv_concat;
-    private ViewPager vp;
 
-    private List<Fragment> data=new ArrayList<Fragment>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.activity_main, container, false);
-            initView(convertView);
+
         }
         ViewGroup viewParent = (ViewGroup) convertView.getParent();
         if (viewParent != null) {
@@ -45,32 +32,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     }
 
 
-    private void initView(View view) {
-
-        tv_concat_launch.setTextColor(getResources().getColor(R.color.white));
-        tv_concat_join.setTextColor(getResources().getColor(R.color.white));
-        view_concat_join.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-
-        rl_home.setOnClickListener(this);
-        rl_job.setOnClickListener(this);
-        iv_search.setOnClickListener(this);
-        iv_concat.setOnClickListener(this);
 
 
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent intent=null;
-        switch (v.getId()) {
-
-
-            default:
-                break;
-        }
-        if (intent!=null)
-        {
-            startActivity(intent);
-        }
-    }
 }
--- app/src/main/java/com/zhanglemeng/www/f2503/base/MainActivity.java
+++ app/src/main/java/com/zhanglemeng/www/f2503/base/MainActivity.java
@@ -2,13 +2,10 @@ package com.zhanglemeng.www.f2503.base;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTabHost;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import com.zhanglemeng.www.f2503.R;
 
 
 /**
@@ -37,49 +34,49 @@ public class MainActivity extends BaseActivity{
      * 初始化组件
      */
     private void initView() {
-        tabHost = (FragmentTabHost) findViewById(R.id.tabhost);
-        tabHost.setup(this,getSupportFragmentManager(),R.id.realtabcontent);
-        tabHost.getTabWidget().setDividerDrawable(null);
-        // 首页：
-        View vHome = getLayoutInflater()
-                .inflate(R.layout.tab_hostcontent, null);
-        ImageView ivBid = (ImageView) vHome.findViewById(R.id.btn_main);
-        ivBid.setImageResource(R.drawable.tab_home_bg);
-        TextView tvHome = (TextView) vHome.findViewById(R.id.btn_main2);
-        tvHome.setText("首页");
-        TabHost.TabSpec bidTabSpec = tabHost.newTabSpec("tabHome").setIndicator(vHome);
-        tabHost.addTab(bidTabSpec, HomeFragment.class, null);
-
-        // 办公：
-        View vOffice = getLayoutInflater()
-                .inflate(R.layout.tab_hostcontent, null);
-        ImageView ivOffice = (ImageView) vOffice.findViewById(R.id.btn_main);
-        ivOffice.setImageResource(R.drawable.tab_office_bg);
-        TextView tvOffice = (TextView) vOffice.findViewById(R.id.btn_main2);
-        tvOffice.setText("办公");
-        TabHost.TabSpec officeTabSpec = tabHost.newTabSpec("tabOffice").setIndicator(vOffice);
-        tabHost.addTab(officeTabSpec,HomeFragment.class, null);
-
-        // 消息：
-        View vMsg = getLayoutInflater()
-                .inflate(R.layout.tab_hostcontent, null);
-        ImageView ivMsg = (ImageView) vMsg.findViewById(R.id.btn_main);
-        ivMsg.setImageResource(R.drawable.tab_message_bg);
-        TextView tvMsg = (TextView) vMsg.findViewById(R.id.btn_main2);
-        tvMsg.setText("消息");
-        unreadLabel = (TextView) vMsg.findViewById(R.id.unread_msg_number);
-        unreadLabel.setVisibility(View.GONE);
-        TabHost.TabSpec msgTabSpec = tabHost.newTabSpec("tabMsg").setIndicator(vMsg);
-        tabHost.addTab(msgTabSpec, HomeFragment.class, null);
-        //我的
-        View vUser = getLayoutInflater()
-                .inflate(R.layout.tab_hostcontent, null);
-        ImageView ivUser = (ImageView) vUser.findViewById(R.id.btn_main);
-        ivUser.setImageResource(R.drawable.tab_user_bg);
-        TextView tvUser = (TextView) vUser.findViewById(R.id.btn_main2);
-        tvUser.setText("我的");
-        TabHost.TabSpec userTabSpec = tabHost.newTabSpec("tabUser").setIndicator(vUser);
-        tabHost.addTab(userTabSpec, HomeFragment.class, null);
+//        tabHost = (FragmentTabHost) findViewById(R.id.tabhost);
+//        tabHost.setup(this,getSupportFragmentManager(),R.id.realtabcontent);
+//        tabHost.getTabWidget().setDividerDrawable(null);
+//        // 首页：
+//        View vHome = getLayoutInflater()
+//                .inflate(R.layout.tab_hostcontent, null);
+//        ImageView ivBid = (ImageView) vHome.findViewById(R.id.btn_main);
+//        ivBid.setImageResource(R.drawable.tab_home_bg);
+//        TextView tvHome = (TextView) vHome.findViewById(R.id.btn_main2);
+//        tvHome.setText("首页");
+//        TabHost.TabSpec bidTabSpec = tabHost.newTabSpec("tabHome").setIndicator(vHome);
+//        tabHost.addTab(bidTabSpec, HomeFragment.class, null);
+//
+//        // 办公：
+//        View vOffice = getLayoutInflater()
+//                .inflate(R.layout.tab_hostcontent, null);
+//        ImageView ivOffice = (ImageView) vOffice.findViewById(R.id.btn_main);
+//        ivOffice.setImageResource(R.drawable.tab_office_bg);
+//        TextView tvOffice = (TextView) vOffice.findViewById(R.id.btn_main2);
+//        tvOffice.setText("办公");
+//        TabHost.TabSpec officeTabSpec = tabHost.newTabSpec("tabOffice").setIndicator(vOffice);
+//        tabHost.addTab(officeTabSpec,HomeFragment.class, null);
+//
+//        // 消息：
+//        View vMsg = getLayoutInflater()
+//                .inflate(R.layout.tab_hostcontent, null);
+//        ImageView ivMsg = (ImageView) vMsg.findViewById(R.id.btn_main);
+//        ivMsg.setImageResource(R.drawable.tab_message_bg);
+//        TextView tvMsg = (TextView) vMsg.findViewById(R.id.btn_main2);
+//        tvMsg.setText("消息");
+//        unreadLabel = (TextView) vMsg.findViewById(R.id.unread_msg_number);
+//        unreadLabel.setVisibility(View.GONE);
+//        TabHost.TabSpec msgTabSpec = tabHost.newTabSpec("tabMsg").setIndicator(vMsg);
+//        tabHost.addTab(msgTabSpec, HomeFragment.class, null);
+//        //我的
+//        View vUser = getLayoutInflater()
+//                .inflate(R.layout.tab_hostcontent, null);
+//        ImageView ivUser = (ImageView) vUser.findViewById(R.id.btn_main);
+//        ivUser.setImageResource(R.drawable.tab_user_bg);
+//        TextView tvUser = (TextView) vUser.findViewById(R.id.btn_main2);
+//        tvUser.setText("我的");
+//        TabHost.TabSpec userTabSpec = tabHost.newTabSpec("tabUser").setIndicator(vUser);
+//        tabHost.addTab(userTabSpec, HomeFragment.class, null);
     }
 
     @Override
