5f967a820855739721f99832c597c033e75f4d1f
inkun <zhangqiangoffice@163.com>
2016-09-06 23:28:28 +0800

    添加事件监听

--- app/src/main/java/com/zhanglemeng/www/f2503/tenant/activities/AddTenantActivity.java
+++ app/src/main/java/com/zhanglemeng/www/f2503/tenant/activities/AddTenantActivity.java
@@ -2,6 +2,7 @@ package com.zhanglemeng.www.f2503.tenant.activities;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,16 +13,18 @@ import com.zhanglemeng.www.f2503.base.activities.BaseActivity;
  *  新增租客界面
  */
 
-public class AddTenantActivity extends BaseActivity {
+public class AddTenantActivity extends BaseActivity implements View.OnClickListener{
 
     //头部导航栏控件
     private TextView tv_top_title, tv_top_right_text;
 
     //租客信息输入框
     private EditText et_name_input, et_sex_input, et_phone_input, et_id_card;
+    private String str_name, str_sex, str_phone, str_id_card;
 
     //合同信息输入框
     private EditText et_begin_date, et_term_input, et_rent_input, et_payment_method;
+    private String str_begin_date, str_term, str_rent, str_pament_method;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +55,34 @@ public class AddTenantActivity extends BaseActivity {
         tv_top_title.setText(R.string.add_tenant);
         tv_top_right_text.setText(R.string.save);
 
+        //绑定点击事件
+        tv_top_right_text.setOnClickListener(this);
+
+    }
+
+    /**
+     * 检查新增租客的各项数据是否已经准备妥当，非空
+     * @return
+     */
+    private boolean checkData () {
+
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            //点击右上角“保存”按钮，进行数据检查并保存到数据库
+            case R.id.save:
+                if (checkData()) {
+
+                }
+                break;
+            default:
+                break;
+
+        }
     }
 
 
